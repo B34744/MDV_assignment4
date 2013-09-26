38cc1707eb222b307bcdf26f46f464321be8d382
Nicholas <nicholasvanarsdale@gmail.com>
2013-09-25 22:33:16 -0700

    starting out on assignment 4

---
+++ Vanarsdale_Nicholas_assignment4.js
@@ -0,0 +1,31 @@
+// alert("JavaScript works!");
+
+// assignment 4 
+
+// nicholas vanarsdale
+
+// sdi 1309
+
+// Number FUNCTIONS 
+
+var decimal = function(val) {
+	
+	if((val)) {
+		return true;
+	} else {
+		return false;
+	}
+	
+};
+
+// String Functions
+
+
+//Array Functions
+
+
+
+
+//Function calls
+
+console.log("
